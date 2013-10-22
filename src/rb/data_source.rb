3ba5bc56b24fd138aed33077c8d5f6876b54cf56
Toby Crawley <toby@tcrawley.org>
2013-10-22 09:53:12 -0400

    Simplify data source.

--- src/rb/data_source.rb
+++ src/rb/data_source.rb
@@ -1,14 +1,6 @@
 require 'vertx'
 
-data = []
-
-Vertx::FileSystem.read_file_as_buffer('/usr/share/dict/words') do |err, buf|
-  raise err if err
-  
-  Vertx::RecordParser.new_delimited("\n") do |line|
-    data << line.to_s
-  end.input(buf)
-end
+data = File.read('/usr/share/dict/words').split
 
 Vertx.set_periodic(100) do
   Vertx::EventBus.publish(Vertx.config["stream-address"], data.sample)
